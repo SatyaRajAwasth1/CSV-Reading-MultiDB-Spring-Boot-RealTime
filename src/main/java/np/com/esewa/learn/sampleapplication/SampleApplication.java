fc704b2681b6ee700e15d06523d1455d6869fc0b
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-04-03 16:42:09 +0545

    inital commit

---
+++ src/main/java/np/com/esewa/learn/sampleapplication/SampleApplication.java
@@ -0,0 +1,13 @@
+package np.com.esewa.learn.sampleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
