3dc9078e0e42e50ec23a570530016080e09cfe18
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-04-15 19:38:26 +0545

    Converted ProductRespinseDto from class to record for immutability

--- src/main/java/np/com/esewa/learn/sampleapplication/inventory/dto/ProductResponseDto.java
+++ src/main/java/np/com/esewa/learn/sampleapplication/inventory/dto/ProductResponseDto.java
@@ -1,16 +1,11 @@
 package np.com.esewa.learn.sampleapplication.inventory.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
-import java.io.Serializable;
-
-@Getter
-@Setter
-public class ProductResponseDto implements Serializable {
-    private String productName;
-    private String productCode;
-    private long quantity;
-    private long price;
+public record ProductResponseDto (
+        String productName,
+        String productCode,
+        long quantity,
+        long price
+    ) {
 
 }
